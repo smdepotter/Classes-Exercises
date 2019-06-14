8ae788dc080f04d48da967a01e5bd00c85c96057
Sean DePotter <seandep@live.com>
2019-06-14 04:20:44 -0400

    Add project files.

---
+++ Classes Exercises/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Classes_Exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                var stopWatch = new Stopwatch();
+                Console.WriteLine("Hit Enter to Start, Hit Enter Again to Stop");
+                var userInput = Console.ReadLine();
+                stopWatch.Start();
+                Console.ReadLine();
+                var finalTime = stopWatch.Stop();
+                Console.WriteLine(finalTime);
+            }
+        }
+    }
+}
---
+++ Classes Exercises/Stopwatch.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Classes_Exercises
+{
+    public class Stopwatch
+    {
+        private TimeSpan _duration;
+        private DateTime _start;
+
+        public void Start()
+        {
+            _start = DateTime.Now;
+        }
+
+        public TimeSpan Stop()
+        {
+            _duration = (DateTime.Now - _start);
+            return _duration;
+        }
+    }
+}
