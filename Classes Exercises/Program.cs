858e82718d9fb20e789bb6dd26c144d486d38710
Sean DePotter <seandep@live.com>
2019-06-19 01:48:35 -0400

    Removed Unused Variables & Added Exception Handling

--- Classes Exercises/Program.cs
+++ Classes Exercises/Program.cs
@@ -6,15 +6,19 @@ namespace Classes_Exercises
     {
         static void Main(string[] args)
         {
+            /* Design a class called Stopwatch. The job of this class is to simulate a stopwatch.
+             It should provide two methods: Start and Stop. We call the start method ﬁrst, and the stop method next.
+             Then we ask the stopwatch about the duration between start and stop. Duration should be a value in TimeSpan.
+             Display the duration on the console
+             */
             while (true)
             {
                 var stopWatch = new Stopwatch();
                 Console.WriteLine("Hit Enter to Start, Hit Enter Again to Stop");
-                var userInput = Console.ReadLine();
+                Console.ReadLine();
                 stopWatch.Start();
                 Console.ReadLine();
-                var finalTime = stopWatch.Stop();
-                Console.WriteLine(finalTime);
+                Console.WriteLine(stopWatch.Stop());
             }
         }
     }
--- Classes Exercises/Stopwatch.cs
+++ Classes Exercises/Stopwatch.cs
@@ -4,18 +4,23 @@ namespace Classes_Exercises
 {
     public class Stopwatch
     {
-        private TimeSpan _duration;
         private DateTime _start;
+        private bool _hasStarted;
 
         public void Start()
         {
+            if (_hasStarted)
+                throw new InvalidOperationException("You have already started");
             _start = DateTime.Now;
+            _hasStarted = true;
         }
 
         public TimeSpan Stop()
         {
-            _duration = (DateTime.Now - _start);
-            return _duration;
+            if(!_hasStarted)
+                throw new InvalidOperationException("You have not started");
+            _hasStarted = false;
+            return DateTime.Now - _start;
         }
     }
 }
